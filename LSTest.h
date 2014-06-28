79bfa604002b3d804ca24feafd81fdba9c901d82
Aaron Boogaard <aaronboogaard@imac-van-aaron.fritz.box>
2014-06-28 14:32:50 +0200

    added Test class

---
+++ LSTest.h
@@ -0,0 +1,13 @@
+//
+//  LSTest.h
+//  Git Test
+//
+//  Created by Aaron Boogaard on 28-06-14.
+//  Copyright (c) 2014 Aaron Boogaard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSTest : NSObject
+
+@end
---
+++ LSTest.m
@@ -0,0 +1,13 @@
+//
+//  LSTest.m
+//  Git Test
+//
+//  Created by Aaron Boogaard on 28-06-14.
+//  Copyright (c) 2014 Aaron Boogaard. All rights reserved.
+//
+
+#import "LSTest.h"
+
+@implementation LSTest
+
+@end
